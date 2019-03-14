146166d9880d60d0ee1fd2665a7e91459d96e2be
AlexVotry <alexvotry@gmail.com>
2019-03-14 11:36:33 -0700

    find relevant users

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import userReducer from './userReducer';
 
 export default combineReducers({
-    dummy: () => 'temp'
+    posts: postsReducer,
+    users: userReducer
 });
